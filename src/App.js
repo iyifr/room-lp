432b12355c0c6ada828ff66ef4939d7bd42dac61
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2022-11-05 21:12:06 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
 import React from 'react';
 import Header from './Components/Header';
+import HeaderMobile from './Components/HeaderMobile';
 import Image1 from './images/desktop-image-hero-1.jpg';
 import Image2 from './images/desktop-image-hero-2.jpg'
-import Image3 from './images/desktop-image-hero-3.jpg'
+import Image3 from './images/desktop-image-hero-3.jpg';
+import Mobileimg1 from './images/mobile-image-hero-1.jpg'
+import Mobileimg2 from "./images/mobile-image-hero-2.jpg";
+import Mobileimg3 from './images/mobile-image-hero-3.jpg'
 
 const slides = [
   {url : Image1 ,
   textHeader : 'Discover innovative ways to decorate' , 
-  textMain : "We provide unmatched quality , comfort and style for property owners across the country . Our expoerts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love"
+  textMain : "We provide unmatched quality , comfort and style for property owners across the country . Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love"
  }, 
 
   {url : Image2, 
   textHeader : 'We are available all across the globe' , 
-  textMain : "With stores all over the world , it's easy for you to find furniture for your home or place of business. Localy we're in most cities throughout the country. Find the branch nearest to you using our store locator. Any questions? Don't hesitate to contact us today"
+  textMain : "With stores all over the world , it's easy for you to find furniture for your home or place of business. Locally we're in most cities throughout the country. Find the branch nearest to you using our store locator. Any questions? Don't hesitate to contact us today"
  },
 
  {url : Image3, 
   textHeader : 'Manufactured with the best materials' , 
   textMain : "Our modern furniture store provides a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and consistent as possible. With three decades of experience in this industry , we understand what customers want for their home and office"
  }
-
-
 ]
+
+const slidesMobile = [
+  {url : Mobileimg1 ,
+  textHeader : 'Discover innovative ways to decorate' , 
+  textMain : "We provide unmatched quality , comfort and style for property owners across the country . Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love"
+ }, 
+
+  {url : Mobileimg2, 
+  textHeader : 'We are available all across the globe' , 
+  textMain : "With stores all over the world , it's easy for you to find furniture for your home or place of business. Locally we're in most cities throughout the country. Find the branch nearest to you using our store locator. Any questions? Don't hesitate to contact us today"
+ },
+
+ {url : Mobileimg3, 
+  textHeader : 'Manufactured with the best materials' , 
+  textMain : "Our modern furniture store provides a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and consistent as possible. With three decades of experience in this industry , we understand what customers want for their home and office"
+ }
+]
+
 function App() {
   return (
    <>
-   <Header slides = {slides}/>
+  {/*<Header slides = {slides}/>*/}
+  <HeaderMobile slides = {slidesMobile} />
    </>
   );
 }
--- src/Components/HeaderMobile.js
+++ src/Components/HeaderMobile.js
@@ -1,16 +1,18 @@
-import React , {useState} from 'react' 
+import React , {useState, useEffect} from 'react';
+import Navbar from './Navbar';
+import {ReactComponent as Arrow} from '../images/icon-arrow.svg'
+import {ReactComponent as Right} from '../images/icon-angle-right.svg';
+import {ReactComponent as Left} from '../images/icon-angle-left.svg';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
-import {ReactComponent as Right} from '../images/icon-angle-right.svg';
-import {ReactComponent as Left} from '../images/icon-angle-left.svg';
 import Image1 from '../images/image-about-dark.jpg'
 import Image2 from '../images/image-about-light.jpg'
-import {ReactComponent as Arrow} from '../images/icon-arrow.svg'
 import '../styles/main.css'
 
-const Header = ({slides}) => {
-  const [currentIndex , setCurrentIndex] = useState(1)
+const HeaderMobile = ({slides}) => {
+
+     const [currentIndex , setCurrentIndex] = useState(1)
 
 function Forwards () {
   const isLastSlide =  currentIndex === slides.length - 1
@@ -25,52 +27,37 @@ function Backwards () {
   let newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
 
   setCurrentIndex(newIndex)
+}
  
-} 
 
-    return <>
-    {/*Desktop header design*/}
-    <Container fluid = "true">
-      <Row noGutters>
-        <Col lg = {7} sm = {12} style = {{paddingLeft: 0 , paddingRight : 1}}>
-          <section className = "header" style = {{backgroundImage : `url(${slides[currentIndex].url})`, width: '100%',
-           backgroundPosition : 'center' , backgroundSize: 'cover', height: '82vh'}} >
-            <nav>
-            <div className='nav-links'>
-            <ul>
-              <li><h4>Room</h4></li>
-              <li><a href = '*'>Home</a></li>
-              <li><a href = '*'>Shop</a></li>
-              <li><a href = '*'>About</a></li>
-              <li><a href = '*'>Contact</a></li>
-            </ul>
-            </div>
-            </nav>  
-          </section>
-        </Col>
 
-        <Col lg = {5} sm = {12}>
-            <section className = 'SlideText'>
+  return (
+    <>
+    <section className = "mobileTop"  style = {{backgroundImage : `url(${slides[currentIndex].url})`, width: '100%',
+           backgroundPosition : 'center' , backgroundSize: 'cover', height: '53vh', marginTop : '0', paddingTop: '5vh'}} >
+            <h2 className='mobileHeader'>Room</h2>
+     </section>
+
+     <div className='buttonsMobile'>
+            <button onClick = {Backwards} > <Left /> </button>
+            <button onClick = {Forwards} > <Right /> </button>
+          </div>
+
+      <section className = 'SlideText'>
           <h1>{slides[currentIndex].textHeader}</h1>
           <p>{slides[currentIndex].textMain}</p>
           <h3> Shop now </h3> <Arrow  width = '150' height = '43' viewBox='-62 16 115 1' style = {{marginLeft : '120px', marginTop : '-32px'}}/>
         </section>
-        <div className='buttons'>
-            <button onClick = {Backwards} > <Left /> </button>
-            <button onClick = {Forwards} > <Right /> </button>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+        
 
-    <Container fluid = 'true'>
+          <Container fluid = 'true'>
       <Row noGutters>
-        <Col lg = {4} sm = {12}>
+        <Col lg = {4} sm = {12} md = {12}>
            <section className = "about1" style = {{backgroundImage : `url(${Image1})`, width: '100%',
            backgroundPosition : 'center' , backgroundSize: 'cover', height: '40vh'}} ></section>
         </Col>
 
-        <Col lg = {4} sm = {12}>
+        <Col lg = {4} sm = {12} md = {12}>
         <section className = 'aboutText'>
           <h3> About our furniture</h3>
           <p>Our multifunctional collection blends design and function to suit your individual taste. Make each room unique , or pick a cohesive theme that best fits
@@ -80,15 +67,16 @@ function Backwards () {
         </section>
         </Col>
 
-        <Col lg = {4} sm = {12}>
+        <Col lg = {4} sm = {12} md = {12}>
            <section className = "header" style = {{backgroundImage : `url(${Image2})`, width: '100%',
            backgroundPosition : 'center' , backgroundSize: 'cover', height: '40vh'}} ></section>
         </Col>
       </Row>
     </Container>
+    
+      
     </>
-
+  )
 }
 
-
-export default Header
+export default HeaderMobile
